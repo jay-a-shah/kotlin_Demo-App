70e69a319984702d5f0fcfe132f1b83458d48ea6
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-01-13 11:22:41 +0530

    TE5-T406 Relative Layout and Other Layouts Added

--- app/src/main/java/com/example/demoappkotlin/differentLayout/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/example/demoappkotlin/differentLayout/ConstraintLayoutActivity.kt
@@ -3,23 +3,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import com.example.demoappkotlin.R
 
-class LinearLayoutActivity : AppCompatActivity() {
+class ConstraintLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear_layout)
-        var spinner: Spinner = findViewById(R.id.spinnerLinearLayout)
+        setContentView(R.layout.activity_constraint_layout)
+        var spinner: Spinner = findViewById(R.id.spinnerConstraintLayout)
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                Toast.makeText(this@LinearLayoutActivity, " ${adapterView?.getItemAtPosition(position).toString()}",
-                        Toast.LENGTH_SHORT).show()
-            }
-            override fun onNothingSelected(p0: AdapterView<*>?){
-                //Implemented When Needed
+                Toast.makeText(applicationContext, " ${adapterView?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_SHORT).show() }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //will br implemented when needed
             }
         }
     }
--- app/src/main/java/com/example/demoappkotlin/differentLayout/LayoutActivity.kt
+++ app/src/main/java/com/example/demoappkotlin/differentLayout/LayoutActivity.kt
@@ -9,8 +9,28 @@ class LayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout)
-        val linearLayoutBtn = findViewById<Button>(R.id.btnLinearLayout)
-        linearLayoutBtn.setOnClickListener {
+        var btnRelativeLayout = findViewById<Button>(R.id.btnRelativeLayout)
+        btnRelativeLayout.setOnClickListener {
+            val intent = Intent(applicationContext, RelativeLayoutActivity::class.java)
+            startActivity(intent)
+        }
+        val btnConstraintLayout = findViewById<Button>(R.id.btnConstraintLayout)
+        btnConstraintLayout.setOnClickListener {
+            val intent = Intent(applicationContext, ConstraintLayoutActivity::class.java)
+            startActivity(intent)
+        }
+        val btnGridLayout = findViewById<Button>(R.id.btnGridLayout)
+        btnGridLayout.setOnClickListener {
+            val intent = Intent(applicationContext, GridLayoutActivity::class.java)
+            startActivity(intent)
+        }
+        val btnFrameLayout = findViewById<Button>(R.id.btnFrameLayout)
+        btnFrameLayout.setOnClickListener {
+            val intent = Intent(applicationContext, FrameLayoutActivity::class.java)
+            startActivity(intent)
+        }
+        val btnLinearLayout = findViewById<Button>(R.id.btnLinearLayout)
+        btnLinearLayout.setOnClickListener {
             val intent = Intent(applicationContext, LinearLayoutActivity::class.java)
             startActivity(intent)
         }
