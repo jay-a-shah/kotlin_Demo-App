1a61e768a838d953e2c18718ce34a6a6238fd3e8
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-02-24 15:34:58 +0530

    TE5-T422 View Pager Added

---
+++ app/src/main/java/com/example/demoappkotlin/listView_ViewPager/Adapters/ViewPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.demoappkotlin.listView_ViewPager.Adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.demoappkotlin.R
+import com.example.demoappkotlin.listView_ViewPager.Fragments.MyFavourites
+import com.example.demoappkotlin.listView_ViewPager.Fragments.Recent
+import com.example.demoappkotlin.utils.ONE
+import com.example.demoappkotlin.utils.TWO
+import com.example.demoappkotlin.utils.ZERO
+
+class ViewPagerAdapter(val context: Context, fragManager: FragmentManager): FragmentPagerAdapter(fragManager){
+
+    override fun getCount(): Int {
+        return TWO
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            ZERO -> { return Recent() }
+            ONE -> { return MyFavourites() }
+            else -> { return Recent() }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            ZERO -> { return context.getString(R.string.tab_title_recent)}
+            ONE -> { return  context.getString(R.string.tab_title_myFavourite) }
+        }
+        return super.getPageTitle(position)
+    }
+}
---
+++ app/src/main/java/com/example/demoappkotlin/listView_ViewPager/Adapters/ViewPagerThreeAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.demoappkotlin.listView_ViewPager.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.demoappkotlin.R
+
+class ViewPagerThreeAdapter(val img: List<Int>) : RecyclerView.Adapter<ViewPagerThreeAdapter.ViewPagerHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager, parent, false)
+        return ViewPagerHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerHolder, position: Int) {
+        holder.ivSliderImage.setImageResource(img[position])
+    }
+
+    override fun getItemCount(): Int {
+        return img.size
+    }
+
+    class ViewPagerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val ivSliderImage = itemView.findViewById<ImageView>(R.id.ivSliderImage)
+    }
+}
