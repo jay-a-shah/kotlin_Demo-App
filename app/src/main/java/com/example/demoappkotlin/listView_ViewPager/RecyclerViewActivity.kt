545bf4faebad6cddc70cdddcdaa2f27cf931e33b
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-03-14 19:06:46 +0530

    TE5-T605 Web View and Horizontal Recycler View

--- app/src/main/java/com/example/demoappkotlin/listView_ViewPager/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/demoappkotlin/listView_ViewPager/RecyclerViewActivity.kt
@@ -2,6 +2,7 @@ package com.example.demoappkotlin.listView_ViewPager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,13 +12,18 @@ import com.example.demoappkotlin.listView_ViewPager.ModelClasses.ModelRecycle
 
 class RecyclerViewActivity : AppCompatActivity() {
     lateinit var myRecyclerAdapter: RecycleAdapter
+    lateinit var recyclerView: RecyclerView
+    lateinit var horizontalRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+        val searchView = findViewById<SearchView>(R.id.searchBar)
+        recyclerView = findViewById(R.id.recyclerViewLayout)
+        horizontalRecyclerView = findViewById(R.id.horizontalRecyclerView)
         var containList = ArrayList<ModelRecycle>()
-        myRecyclerAdapter = RecycleAdapter(containList)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewLayout)
+        var filterArrayList = ArrayList<ModelRecycle>()
+        filterArrayList.addAll(containList)
         recyclerView.layoutManager = LinearLayoutManager(this)
         containList.apply {
             add(ModelRecycle("Android", R.drawable.android, false))
@@ -33,8 +39,44 @@ class RecyclerViewActivity : AppCompatActivity() {
             add(ModelRecycle("Kotlin", R.drawable.kotlin, false))
             add(ModelRecycle("larvel", R.drawable.larvel, false))
         }
-        recyclerView.apply { addItemDecoration(DividerItemDecoration(baseContext, (recyclerView.layoutManager as LinearLayoutManager).orientation))
+        setAdapter(containList)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterArrayList.clear()
+                if (newText?.lowercase()?.isNotEmpty() == true) {
+                    var list = containList.filter {
+                        it.name.lowercase().contains(newText.toString())
+                    } as ArrayList<ModelRecycle>
+                    filterArrayList.addAll(list)
+                    setAdapter((filterArrayList))
+                } else {
+                    filterArrayList.apply {
+                        clear()
+                        addAll(containList)
+                    }
+                    setAdapter(filterArrayList)
+                }
+                return false
+            }
+        })
+    }
+
+    private fun setAdapter(arrayList: ArrayList<ModelRecycle>) {
+        myRecyclerAdapter = RecycleAdapter(arrayList)
+        recyclerView.apply {
+            addItemDecoration(
+                DividerItemDecoration(
+                    baseContext,
+                    (recyclerView.layoutManager as LinearLayoutManager).orientation
+                )
+            )
             adapter = myRecyclerAdapter
         }
+        horizontalRecyclerView.adapter = myRecyclerAdapter
     }
 }
